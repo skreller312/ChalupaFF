33273349473d72b032e0b12486a00676554dfc00
Stephen Kreller <69259004+skreller312@users.noreply.github.com>
2026-09-21 11:47:53 -0500

    Add config.mjs

---
+++ config.mjs
@@ -0,0 +1,10 @@
+export const CONFIG = {
+  season: 2026,
+  sleeperUsername: "Skrelleton",
+  sleeperLeagueIds: ["1400242244467884032","1357387667003678720","1340499213041020928","1389328755817730048","1389750900079222784"],
+  espnLeagues: [
+    { id: "875459", teamId: 12, season: 2026 },
+    { id: "1841110834", teamId: 1, season: 2026 },
+    { id: "84938142", teamId: 1, season: 2026 }
+  ]
+};
